7db805f67cc46f9aea0fc9accb77c921a1dfd296
iflytek_xuchenli <clxu7@iflytek.com>
2024-03-27 11:53:44 +0800

    feat: 弹窗

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,7 +17,67 @@ module.exports = {
           s.parentNode.insertBefore(hm, s);
         })();
       `
-    ] : ''
+    ] : '',
+    ['script', {}, `
+      var myDialogHtml = 
+        '<div class="myDialog-content">' +
+        '  <div>该组件已弃用，请向往查看最新的组件文档。</div>' +
+        '  <div>' +
+        '    <a href="https://qweaa.github.io/lattice-lottery-new/?from=old">lattice-lottery-new</a>' +
+        '  </div>' +
+        '  <div>感谢您的支持~</div>' +
+        '  <div>' +
+        '    <a href="https://qweaa.github.io/lattice-lottery-new/?from=old" class="myDialog-btn">Go~</a>' +
+        '  </div>' +
+        '</div>'
+      var myDialog = document.createElement("div")
+      myDialog.className = 'myDialog'
+      myDialog.innerHTML = myDialogHtml
+
+      document.body.appendChild(myDialog)
+    `],
+    ['style', {}, `
+      .myDialog{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.6);
+        z-index: 1000;
+      }
+      .myDialog-content {
+        width: 360px;
+        padding-top: 40px;
+        padding-bottom: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        font-size: 14px;
+        color: #666;
+        background-color: white;
+        border-radius: 6px;
+        position: fixed;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+      }
+      .myDialog-content > div {
+        margin-bottom: 16px;
+      }
+      .myDialog-btn {
+        display: block;
+        width: 120px;
+        height: 32px;
+        text-align: center;
+        background-color: #67C23A;
+        color: white;
+        border-radius: 4px;
+        line-height: 32px;
+        text-decoration: none;
+      }
+    `]
   ],
   locales: {
     '/': {
